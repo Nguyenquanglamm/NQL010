c63cc60cb16bbb863d1cb4cf7fbac036507e1baf
quanglam01111999@ gmail.com <quanglam01111999@gmail.com>
2022-03-01 08:34:10 +0700

    commit demo

---
+++ NQLBaiThucHanh010/NQLBaiThucHanh010/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NQLBaiThucHanh010.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Masinhvien ,string Tensinhvien)
+        {
+            ViewBag.Message = Masinhvien + "   " + Tensinhvien;
+            return View();
+        }
+    }
+}
